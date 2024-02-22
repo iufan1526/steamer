8d181d45ddf0d3fc534faa4fea55b72fbb87257c
kim <iufan1526@gmail.com>
2024-02-22 18:10:44 +0900

    config , db, typeorm 패키지 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GamesModule } from './games/games.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [GamesModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        GamesModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
