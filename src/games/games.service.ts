3d15c8dc91d75f9a52eb629a743f9d6b14cf2c90
kim <iufan1526@gmail.com>
2024-02-26 18:31:37 +0900

    feat: 게임 정보 추출 부분완성

--- src/games/games.service.ts
+++ src/games/games.service.ts
@@ -1,13 +1,17 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { GET_GAMES_BY_STEAM_URL, GET_GAME_DETAIL_BY_STEAM_URL } from './const/api-url.const';
-import { RawGames } from './dto/raw-game.dto';
 import { GamesEntity } from './entities/games.entity';
-import { CreateGame } from './dto/create-game.dto';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class GamesService {
-    constructor(private readonly httpService: HttpService) {}
+    constructor(
+        private readonly httpService: HttpService,
+        @InjectRepository(GamesEntity)
+        private readonly gamesRepository: Repository<GamesEntity>,
+    ) {}
 
     async createGames() {
         const rawGames = await this.httpService.axiosRef.get(GET_GAMES_BY_STEAM_URL);
@@ -16,22 +20,36 @@ export class GamesService {
 
         for (let i = 0; i < 40; i++) {
             if (apps[i].name) {
-                const gameDetail = await this.httpService.axiosRef.get(GET_GAME_DETAIL_BY_STEAM_URL + apps[i].appid);
+                const { data } = await this.httpService.axiosRef.get(`${GET_GAME_DETAIL_BY_STEAM_URL}${apps[i].appid}`);
 
-                if (gameDetail.data[apps[i].appid].success && !gameDetail.data[apps[i].appid].data.is_free) {
-                    const { data } = gameDetail.data[apps[i].appid];
-                    // const newObj = new CreateGame({
-                    //     name: data.name,
-                    //     appId: data.steam_appid,
-                    // });
-                    console.log({
-                        name: data.name,
-                        appId: data.steam_appid,
-                        images: {
-                            header_image: data.steam_appid,
-                        },
-                        minimumRequirement: data.pc_requirements.minimum,
+                if (
+                    data[apps[i].appid].success &&
+                    !data[apps[i].appid].data.is_free &&
+                    !data[apps[i].appid].data.release_date['coming_soon']
+                ) {
+                    const game = data[apps[i].appid].data;
+
+                    const newObj = this.gamesRepository.create({
+                        name: game.name,
+                        appId: game.steam_appid,
+                        thumnail: game.header_image,
+                        score: game.metacritic ? game.metacritic.score : 0,
+                        recommentation: game.recommendations ? game.recommendations.total : 0,
+                        isWindow: game.platforms.windows,
+                        isLinux: game.platforms.linux,
+                        isMac: game.platforms.mac,
                     });
+
+                    /**
+                     * 멀티플레이 지원여부
+                     */
+                    if (game.categories) {
+                        game.categories.forEach((cate: { id: number }) => {
+                            cate.id === 1 ? (newObj.isPossibleMulti = true) : (newObj.isPossibleMulti = false);
+                        });
+                    }
+
+                    console.log(newObj);
                 }
             }
         }
