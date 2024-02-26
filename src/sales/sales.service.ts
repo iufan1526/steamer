3552582fa6a6ad667ab31ace872a1c1ed0a06eb4
Beardevelope <mck84835182@gmail.com>
2024-02-26 18:31:53 +0900

    feat : sales ednpoint test

--- src/sales/sales.service.ts
+++ src/sales/sales.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { SalesEntity } from './entities/sales.entity';
 
 @Injectable()
-export class SalesService {}
+export class SalesService {
+    constructor(private readonly salesRepository: Repository<SalesEntity>) {}
+
+    async getAllGames() {
+        await this.salesRepository.find();
+    }
+}
