3356d7abe88bdf2fbe3b4ceb313868bf64c66f8b
kim <iufan1526@gmail.com>
2024-02-29 21:28:05 +0900

    게임 저장 로직 추가

--- src/games/games.controller.ts
+++ src/games/games.controller.ts
@@ -1,21 +1,18 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { GamesService } from './games.service';
-import { HttpService } from '@nestjs/axios';
 
 @Controller('games')
 export class GamesController {
-    constructor(
-        private readonly gamesService: GamesService,
-        private readonly httpService: HttpService,
-    ) {}
+    constructor(private readonly gamesService: GamesService) {}
 
     @Get()
     async test() {
-        this.gamesService.createGames();
+        //this.gamesService.rawGamesSave();
+        this.gamesService.saveGamesProcess();
     }
 
-    @Get('games')
-    async getAllGames(@Query('orderBy') orderBy: string, @Query('genre') genre: string) {
-        return this.gamesService.getAllgames(orderBy, genre);
+    @Get('appId')
+    async test1() {
+        //return this.gamesService.getAppId();
     }
 }
